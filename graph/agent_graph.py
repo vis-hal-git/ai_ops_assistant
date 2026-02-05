1899039c8c7256cad668b8a7c8632c4d332c0705
vis-hal-git <164080515+vis-hal-git@users.noreply.github.com>
2026-02-06 01:05:51 +0530

    Update agent_graph.py

--- graph/agent_graph.py
+++ graph/agent_graph.py
@@ -1,7 +1,16 @@
 from langgraph.graph import StateGraph
 
+from typing import TypedDict, List, Annotated
+import operator
+
+class AgentState(TypedDict):
+    task: str
+    plan: dict
+    data: List[dict]
+    final_answer: str
+
 def build_graph(planner, executor, verifier):
-    graph = StateGraph(dict)
+    graph = StateGraph(AgentState)
 
     graph.add_node("planner", planner)
     graph.add_node("executor", executor)
@@ -10,5 +19,8 @@ def build_graph(planner, executor, verifier):
     graph.set_entry_point("planner")
     graph.add_edge("planner", "executor")
     graph.add_edge("executor", "verifier")
+    graph.set_finish_point("verifier")
 
     return graph.compile()
+
+
